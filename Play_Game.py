c86c03111a83bd8d024fbed48d2cf48164517b20
ljsnell <ljsnell7@gmail.com>
2021-02-06 01:06:00 -0600

    Added shopping display.

--- Play_Game.py
+++ Play_Game.py
@@ -1,9 +1,18 @@
 import Human_Character
 import Battle
+import Weapon
 from Game_States import GAME_OVER, CHOOSING_PATH, IN_BATTLE, SHOPPING
 from GraveStone import displayGrave
 
 battle_station = Battle.Battle()
+weapon_for_sale1 = Weapon.Weapon('smol sword', 5, [], 9)
+weapon_for_sale2 = Weapon.Weapon('medium sword', 5, [], 9)
+weapon_for_sale3 = Weapon.Weapon("'Honestly too big' sword", 5, [], 9)
+
+weapon_list = []
+weapon_list.append(weapon_for_sale1)
+weapon_list.append(weapon_for_sale2)
+weapon_list.append(weapon_for_sale3)
 
 enemy_character = Human_Character.HumanCharacter("Evil Harold", 5, 10, 5, 3, 9)
 player_character = Human_Character.HumanCharacter("Harold", 7, 10, 7, 2, 0)
@@ -18,8 +27,12 @@ while(game_status != GAME_OVER):
         game_status = IN_BATTLE
         game_status, player_character = battle_station.combat(
             enemy_character, player_character, game_status)
-    elif choice == 's':
+    elif choice == 's':        
         # Shopping
         game_status = SHOPPING
+        for (index, weapon) in enumerate(weapon_list):
+            print(index+1)
+            weapon.display()
+
 
 displayGrave()
