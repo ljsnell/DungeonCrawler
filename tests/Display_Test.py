b0268fbdc0c8c2d64546b209f39824f878304fdd
ljsnell <ljsnell7@gmail.com>
2021-03-21 01:27:31 -0500

    Test moved to its own file.

--- tests/Display_Test.py
+++ tests/Display_Test.py
@@ -1,4 +1,9 @@
+import sys
+import os
+dir_path = os.path.dirname(os.path.normpath(__file__))
+sys.path.append(os.path.dirname(dir_path))
 from characters import Rat_Character
+
 
 batChar = Rat_Character.RatCharacter(4, 5, 1)
 
