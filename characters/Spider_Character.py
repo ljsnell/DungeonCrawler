10a9f94dcaeb4ef4ccbf411404b80e18c67425d5
ljsnell <ljsnell7@gmail.com>
2021-04-12 19:19:15 -0500

    Initial Commit.

---
+++ characters/Spider_Character.py
@@ -0,0 +1,34 @@
+import random
+
+
+class SpiderCharacter:
+    spider_names = ['Baleful', "Steeple-Legged", "Glistening"]
+    spider_name = random.choice(spider_names)
+
+    def __init__(self, chest):
+        self.name = self.spider_name + ' Spider'
+        self.chest = chest
+        self.exp = 5
+        self.damage = 3
+        self.gold = 4
+
+    def display(self):
+        print('**************************')
+        print('NAME: ' + self.name)
+        print(r'   /\(0 c:' + str(self.chest) + ' 0)/\\')
+        print('**************************')
+
+    def display_defeat(self):
+        print('**************************')
+        print('NAME: ' + self.name)
+        print('   __/(0 c:' + str(self.chest) + ' 0)\\__')
+        print('**************************')
+
+    def determine_damage(self, hit_location, damage):
+        self.chest -= damage
+
+    def is_alive(self):
+        if(self.chest > 0):
+            return True
+        else:
+            return False
